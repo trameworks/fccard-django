d69640ac0f349ff8fbc702817925893b5b4f76b4
Ricardo Brazileiro <rbrazileiro@gmail.com>
2018-11-27 00:07:16 -0300

    update form validation

--- static/js/contact-form-validation.js
+++ static/js/contact-form-validation.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function($) {
 					email: $form.find('#id_email').val(),
 					subject: $form.find('#id_subject').val(),
 					message: $form.find('#id_message').val(),
-					recaptcha: $form.attr("data-sitekey"),
+					recaptcha: $form.find('#id_recaptcha').attr("data-sitekey"),
 					csrfmiddlewaretoken: $form.attr("data-csrf-token")
 				},
 				dataType: 'json',
